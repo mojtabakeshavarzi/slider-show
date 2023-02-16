1f2c379b76c03cd87723decd8954d999f9d6f712
Mojtaba Keshavarzi <mojtabakeshavarzi1998@gmail.com>
2023-02-16 23:32:56 +0330

    First Commit

---
+++ src/Review.js
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import { FaChevronLeft , FaChevronRight , FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+
+function Reviews() {
+
+
+    const [index , setIndex] = useState(0);
+    const {name , job , image , text} = data[index];
+
+    const checkNumber = (number) => {
+        if(number > data.length -1) {
+            return 0;
+        }
+        if(number < 0) {
+            return data.length -1;
+        }
+        return number;
+    }
+
+    const nextSlide = () =>
+    {
+        setIndex((index) => 
+        {
+            let newIndex = index + 1 ;
+            return checkNumber(newIndex);
+        })
+    }
+
+    const prevSlide = () =>
+    {
+        setIndex((index) => 
+        {
+            let newIndex = index - 1 ;
+            return checkNumber(newIndex);
+        })
+    }
+
+    const randomPerson = () =>
+    {
+        let randomNumber = Math.floor(Math.random() * data.length);
+        if(randomNumber === index)
+        {
+            randomNumber = index  + 1;
+        }
+            setIndex(checkNumber(randomNumber));
+    }
+
+    return (
+      <div className="reviews">
+        <img
+        src={image}
+        alt=""
+        />
+        <div>
+            <h4 className="author">
+                {name}
+            </h4>
+            <p className="job">{job}</p>
+            <p className="info">{text}</p>
+            <div className="review-button">
+                <button onClick={nextSlide}><FaChevronRight /></button>
+                <button onClick={prevSlide}><FaChevronLeft /></button>
+            </div>
+            <button className="random-btn" onClick={randomPerson}>Feeling Lucky</button>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Reviews;
+  
---
+++ src/data.js
@@ -0,0 +1,34 @@
+import jake from '../src/img/jake.png';
+
+const reviews = [
+{
+    id : 1 ,
+    name : "Mojtaba",
+    job : "FrontEnd Developer",
+    image : "https://images.unsplash.com/photo-1675625431865-b49f02d779c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    text : "The human heart creates enough pressure to squirt blood 30 feet (9 m). Diet Coke was only invented in 1982. There are more than 1,700 references to gems and precious stones in the King James translation of the Bible. When snakes are born with two heads, they fight each other for food."
+},
+{
+    id : 2 ,
+    name : "Farzad",
+    job : "programmer",
+    image : "https://images.unsplash.com/photo-1651582515679-a3994bb0cdce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    text : `The half-hour series premiered on December 17, 1989, with "Simpsons Roasting on an Open Fire", a Christmas special. "Some Enchanted Evening" was the first full-length episode produced, but it did not broadcast until May 1990, as the last episode of the first season, because of animation problems.`
+},
+{
+    id : 3 ,
+    name : "Shayan",
+    job : "web Designer",
+    image : "https://images.unsplash.com/photo-1675164388740-4461facd2b03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE3fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    text : `Whales are mammals who have no sweat glands, but then since they live in the water, they don't really need them.`
+},
+{
+    id : 4 ,
+    name : "Reza" , 
+    job : "Graphic Designer",
+    image : "https://images.unsplash.com/photo-1564520599940-c06900b727f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGNvZmZlZSUyMGN1cHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60" ,
+    text : `It is not true, as is commonly believed, that bulls have horns and cows do not: the presence of horns depends on the breed, or in horned breeds on whether the horns have been disbudded (conversely, in many breeds of sheep it is indeed only the males which have horns).` 
+}
+];
+
+export default reviews;
